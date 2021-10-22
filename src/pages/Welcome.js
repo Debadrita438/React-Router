7ecd2d06419d3a8a4fc382caac6e2bb0ff224a17
Debadrita Bandyopadhyay <bristibandyopadhyay@gmail.com>
2021-10-22 12:29:37 +0530

    Applying dynamic route, nested route

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,6 +1,11 @@
+import { Route } from "react-router";
+
 const Welcome = () => {
     return(
-        <h1>The Welcome Page</h1>
+        <>
+            <h1>The Welcome Page</h1>
+            <Route path='/welcome/new-user'><p>Welcome New User</p></Route>
+        </>
     )
 }
 
